a5690028c947de678e1a0bcec82bbcf9ac5009ff
Jeroen Keysers <r0840282@student.thomasmore.be>
2021-01-08 22:09:23 +0100

    SwitchItem herkansing

---
+++ src/main/java/com/gildedrose/items/SwitchItem.java
@@ -0,0 +1,24 @@
+package com.gildedrose.items;
+
+import com.gildedrose.core.Item;
+
+public class SwitchItem extends Item {
+
+
+    public SwitchItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        int factor;
+        if (this.sellIn >= 0){
+            factor = 1;
+            this.quality = Math.min(this.quality + factor, 50);
+        } else {
+            factor = 1;
+            this.quality = Math.max(this.quality - factor, 0);
+        }
+    }
+}
+
